8bc4c4acb5fb39e42d05b5654e0760e11ffce2bb
이준혁 <merong245@gmail.com>
2023-07-07 15:14:16 +0900

    feat: admin, dashboard, info domain 추가

--- src/main/java/com/example/springsecurity/controller/SampleController.java
+++ src/main/java/com/example/springsecurity/controller/SampleController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class SampleController {
 
@@ -11,9 +13,34 @@ public class SampleController {
     // handlerAdaptor가 해당 컨트롤러에 동작을 위임함
     // mapping url
     @GetMapping("/")
-    public String index(Model model){
-        // model에 message라는 속성에 Hello Spring Security라는 값을 담아줌
-        model.addAttribute("message","Hello Spring Security");
+    public String index(Model model, Principal principal){
+        if(principal == null) {
+            // model에 message라는 속성에 Hello Spring Security라는 값을 담아줌
+            model.addAttribute("message", "Hello Spring Security");
+        }
+        else{
+            model.addAttribute("message","Hello " + principal.getName());
+        }
         return "index"; // view Resolver가 해석
     }
+    @GetMapping("/info")
+    public String info(Model model){
+        model.addAttribute("message","Info");
+        return "info";
+    }
+
+    @GetMapping("/dashboard")
+    public String info(Model model, Principal principal){
+        model.addAttribute("message","Hello " + principal.getName());
+        return "dashboard";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal){
+        model.addAttribute("message","Hello " + principal.getName());
+        return "admin";
+    }
+
+
+
 }
