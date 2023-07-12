a77b4c0c0954ac58ab7ff4e41959f359f47baffb
이준혁 <merong245@gmail.com>
2023-07-12 21:26:23 +0900

    config: 스프링 시큐리티 config 설정 및 기본 유저 추가 기존에는 WebSecuritConfiguration을 사용했지만 이제 Deprecated 되고  @EnableWebSecurity만으로 설정할 수 있게됨. 또 PassordEncoder에 암호화 방식을 {암호화 명}password 등으로 표현해야함 -> PasswordEncoder 필수

---
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.example.springsecurity.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .mvcMatchers("/", "/info", "/account/**").permitAll()
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated();
+        http.formLogin();
+        http.httpBasic();
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}pass123")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}pass123")
+                .roles("ADMIN")
+                .build();
+
+        List<UserDetails> userDetails = List.of(user, admin);
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
