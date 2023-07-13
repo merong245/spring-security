cf54033c5f679db48a2c7dd4299c0590414c56a9
이준혁 <merong245@gmail.com>
2023-07-13 23:16:18 +0900

    feat : spring datajpa, h2, lombok 의존성 추가 feat : account 엔티티, 서비스, 레포지토리 구현

---
+++ src/main/java/com/example/springsecurity/account/Account.java
@@ -0,0 +1,28 @@
+package com.example.springsecurity.account;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    @Id @GeneratedValue
+    private Integer id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String role;
+}
