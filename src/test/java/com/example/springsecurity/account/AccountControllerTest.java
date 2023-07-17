9384ef74e2bb6cbae1219b4d2b0dc4b42ff2a662
이준혁 <merong245@gmail.com>
2023-07-18 00:21:59 +0900

    test : 로그인 테스트 코드 추가

--- src/test/java/com/example/springsecurity/account/AccountControllerTest.java
+++ src/test/java/com/example/springsecurity/account/AccountControllerTest.java
@@ -7,12 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,6 +24,12 @@ class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private AccountRepository accountRepository;
 
     @Test
     @WithAnonymousUser
@@ -37,7 +43,7 @@ class AccountControllerTest {
     @Test
     @WithUser
     @DisplayName("로그인된 상태의 유저의 인덱스 페이지 정상 방문")
-    public void index_user() throws Exception{
+    public void index_user() throws Exception {
         mockMvc.perform(get("/"))
                 .andDo(print())
                 .andExpect(status().isOk());
@@ -57,7 +63,7 @@ class AccountControllerTest {
     @Test
     @WithUser
     @DisplayName("로그인된 상태의 유저의 관리자 페이지 방문 실패 - 권한 거부 ")
-    public void admin_page_with_user() throws Exception{
+    public void admin_page_with_user() throws Exception {
         mockMvc.perform(get("/admin"))
                 .andDo(print())
                 .andExpect(status().isForbidden());
@@ -68,11 +74,45 @@ class AccountControllerTest {
     @Test
     @WithAdmin
     @DisplayName("로그인된 상태의 관리자의 관리자 페이지 방문 성공")
-    public void admin_page_with_admin() throws Exception{
+    public void admin_page_with_admin() throws Exception {
         mockMvc.perform(get("/admin"))
                 .andDo(print())
                 .andExpect(status().isOk());
 
+    }
+
+    @Test
+    @DisplayName("올바른 계정으로 로그인")
+    public void login_success() throws Exception {
+        String username = "junhyeok";
+        String pwd = "pwd";
+        Account user = createUser(username, pwd);
+        System.out.println(user);
+
+        mockMvc.perform(formLogin().user(user.getUsername()).password(pwd))
+                .andDo(print())
+                .andExpect(authenticated());
+    }
+
+    @Test
+    @DisplayName("다른 계정으로 로그인")
+    public void login_fail() throws Exception {
+        String username = "junhyeok";
+        String pwd = "pwd";
+        Account user = createUser(username, pwd);
+
+        mockMvc.perform(formLogin().user(user.getUsername()+"123").password(pwd))
+                .andDo(print())
+                .andExpect(unauthenticated());
+    }
+
+    private Account createUser(String username, String pwd) {
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(pwd);
+        account.setRole("USER");
+
+        return accountService.createAccount(account);
     }
 
 }
