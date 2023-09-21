c366d6300db7e077be49350af50043fda250ebc6
이준혁 <merong245@gmail.com>
2023-09-21 22:06:28 +0900

    feat : 메서드 시큐리티 계층형 인가 설정

--- src/main/java/com/example/springsecurity/config/MethodSecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/MethodSecurityConfig.java
@@ -1,2 +1,26 @@
-package com.example.springsecurity.config;public class MethodSecurityConfig {
+package com.example.springsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.access.vote.RoleHierarchyVoter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+/*
+    메서드 시큐리티를 사용하기 위한 설정
+ */
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
+public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+    @Override
+    protected AccessDecisionManager accessDecisionManager() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        AffirmativeBased accessDecisionManager = (AffirmativeBased) super.accessDecisionManager();
+        accessDecisionManager.getDecisionVoters().add(new RoleHierarchyVoter(roleHierarchy));
+        return accessDecisionManager;
+    }
 }
